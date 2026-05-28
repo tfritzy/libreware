690c22e77d7557fc5edd4cbfb104206272e70764
Tyler <tfritzy@gmail.com>
2026-05-28 08:42:57 -0500

    in list sorting

--- kanban/site/src/components/Task.tsx
+++ kanban/site/src/components/Task.tsx
@@ -30,16 +30,18 @@ export const TaskComponent = ({
     <Draggable key={task.id} draggableId={task.id} index={index}>
       {(provided, snapshot) => (
         <div
+          className="py-1"
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           style={getStyle(provided.draggableProps.style, snapshot)}
-          className="flex space-x-2 mb-2 items-center bg-zinc-800 rounded-lg px-3 py-2 cursor-pointer hover:brightness-125 transition-colors duration-[15]"
         >
-          <div
-            className={`rounded-full h-2 w-2 ${task.color || "bg-zinc-500"}`}
-          />
-          <div>{task.name}</div>
+          <div className="flex space-x-2 items-center bg-zinc-800 rounded-lg px-3 py-2 hover:brightness-125 transition-colors duration-[15]">
+            <div
+              className={`rounded-full h-2 w-2 ${task.color || "bg-zinc-500"}`}
+            />
+            <div>{task.name}</div>
+          </div>
         </div>
       )}
     </Draggable>
